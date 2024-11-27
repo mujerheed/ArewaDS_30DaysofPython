660563ff8d9dbd19831d21237475c65fc54908bd
Abdurrazaq Jibril <mujerheed20@gmail.com>
2024-11-27 13:18:23 +0100

    Added Module 1 exercise: Introduction

--- 01_Module_Introduction/helloworld.py
+++ 01_Module_Introduction/helloworld.py
@@ -1,23 +1,42 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(3 + 2)   # addition(+)
-print(3 - 2)   # subtraction(-)
-print(3 * 2)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+print(3 + 4)   # addition(+)
+print(3 - 4)   # subtraction(-)
+print(3 * 4)   # multiplication(*)
+print(3 / 4)   # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 % 4)   # modulus(%)
+print(3 // 4)  # Floor division operator(//)
+
+# Working with strings
+print("My name is Abdurrazaq Jibril Baba")
+print("My family name is Mallam Baba hadejia")
+print("My country is Nigeria")
+print("I am enjoying 30 days of python")
+print("I love Arewa Data Science Fellowship")
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
-print(type(3 == 3))              # Bool
-print(type(3 >= 3))              # Bool
+print(type(10))                 # Int
+print(type(9.8))                # Float
+print(type(3.14))               # Float
+print(type(4 - 4j))              # Complex
+print(type(['Asabeneh', 'Python', 'Finland']))           # List
+print(type('Abdurrazaq Jibril'))          # String
+print(type('Mallam Baba'))          # String
+print(type('Nigeria'))          # String
+
+print(type(27))             # Integer
+print(type(20.24))          # Float
+print(type(1 + 1j))         # Complex
+print(type("Python"))       # String
+print(type({'nick_name':'mujerheed', 'prog': 'Data Science'})) # Dictionary
+print(type({9.3, 3.14, 2.6}))    # Set
+print(type((9.3, 3.14, 2.6)))    # Tuple
+print(type(7 == 7))              # Bool
+print(type(7 <= 3))              # Bool
+
+# Euclidian Distance
+euc_dis = ((10 - 2) + (8 - 3)) ** 0.5
+print("Euclidian distance = ", euc_dis)
